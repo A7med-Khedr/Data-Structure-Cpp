b6cf31be53f6f69f9416683ebb1f9fb6ae21fe26
Ahmed Khedr <ahmedkhedr018@gmail.com>
2024-05-04 01:46:29 +0300

    delete old photo

--- Linked List/Linked_List.cpp
+++ Linked List/Linked_List.cpp
@@ -2,19 +2,21 @@
 #include <cstdlib> // To Use malloc Function
 using namespace std;
 
-class Node
-{
-public:
-  double data;
-  Node *next; // Pointer To Reference The Next Data In Linked List
-  Node(double data)
-  {
-    this->data = data;    // To Assign Value In Function To Data In Node
-    this->next = nullptr; // To Assign Next Data Equal null Always
-  }
-};
+template <class T>
 class Linked_List
 {
+  class Node
+  {
+  public:
+    T data;
+    Node *next; // Pointer To Reference The Next Data In Linked List
+    Node(T data)
+    {
+      this->data = data;    // To Assign Value In Function To Data In Node
+      this->next = nullptr; // To Assign Next Data Equal null Always
+    }
+  };
+
 private:                // Because Head And Tail Must Not Access By User
   Node *head = nullptr; // Head pointer To Reference Always In First Element In Linked List
   Node *tail = nullptr; // Tail Pointer To Reference Always In Last Element In Linked List
@@ -25,14 +27,15 @@ private:                // Because Head And Tail Must Not Access By User
   }
 
 public:
-  void Insert_Node_At_Beginning(double value)
+  void Insert_Node_At_Beginning(T value)
   {                                   // Method To Add Element In First
     Node *New_Node = new Node(value); // Create New Node And Assign Value To Data
-    if (head == NULL)                 // Check Head Equal Null ? Mean Value Is First Element In List : There Is Many Element
+    if (head == nullptr)              // Check Head Equal Null ? Mean Value Is First Element In List : There Is Many Element
     {
       head = New_Node; // Assign Head Pointer To Reference The Data
       tail = New_Node; // And Assign Tail Pointer To Reference The Data
       // Because The Data Is The Alone Element In Linked List
+      return;
     }
     else
     {                        // Mean The Linked List Is Not Null
@@ -43,7 +46,7 @@ public:
 
   // ===================================================
 
-  void Insert_Node_At_End(double value)
+  void Insert_Node_At_End(T value)
   {                                   // Method To Add Element In End
     Node *New_Node = new Node(value); // Create New Node And Assign Value To Data
     if (head == nullptr)              // Check Head Equal Null ? Mean Value Is First Element In List : There Is Many Element
@@ -62,7 +65,49 @@ public:
 
   // ===================================================
 
-  void Remove_Node(double value) // Function To Remove Node
+  void Insert_At_Position(T value, int pos)
+  { // Function To Insert Node In Any Position In Linked List
+    if (pos < 0)
+    { // If User Input Number Less 0 print This
+      cout << "Invalid Position\n";
+    }
+
+    if (pos == 0 || head == nullptr)
+    {                                  // Check If You Need Add Element At Beginning Linked List
+      Insert_Node_At_Beginning(value); // Call This Function And Get Value As A Parameter
+      return;
+    }
+
+    Node *New_Node = new Node(value); // Create New Node To Store Element
+    Node *current = head;             // Create Pointer To Loop In Linked List
+    int current_position = 0;         // Use This To Check Current Pointer Not Exceeds Specific Position
+
+    while (current->next != nullptr && current_position < pos - 1)
+    {                          // Check Current Not Arrive Last Element And current_position Is Valid
+      current = current->next; // Increment Loop
+      current_position++;      // Increment current_position Plus 1 Each Loop Is True
+    }
+
+    if (current->next == nullptr && current_position < pos - 1)
+    { // If Input By User Exceed Length For Linked List Print This
+      cout << "Position: " << pos << " Exceeds The Size Of The Linked List\n";
+      cout << "Size of Linked List Is: " << this->Get_Length() << "\n";
+      delete New_Node; // Delete New Node To Free Memory
+      return;
+    }
+    // If Valid Position
+    New_Node->next = current->next; // Next Pointer To New Node Refer To Next To Current
+    current->next = New_Node;       // Next Pointer To Current Refer To New Node
+
+    if (New_Node->next == nullptr)
+    {                  // Check If Insert Node At End
+      tail = New_Node; // Update Tail To New Node
+    }
+  }
+
+  // ===================================================
+
+  void Remove_Node(T value) // Function To Remove Node
   {
     if (head == nullptr) // Check List Is Empty Or No
     {
@@ -97,7 +142,7 @@ public:
       return;
     }
 
-    if (current = tail) // Current Equal Last Element
+    if (current == tail) // Current Equal Last Element
     {
       perv->next = nullptr; // Element Before Tail Pointer To Null Because It Became Last Element
       tail = perv;          // Move Tail To Last Element
@@ -145,23 +190,24 @@ public:
   // ===================================================
 };
 
-class Double_Node
-{
-public:
-  double data;       // Data Initial By User
-  Double_Node *next; // Pointer To Reference Next Node
-  Double_Node *prev; // Pointer To Reference Pervious Node
-
-  Double_Node(double value)
-  {                       // Constructor To Assign Value In Function  For Data In Double_Node
-    this->data = value;   // Assign Value In Function For Data In Class
-    this->next = nullptr; // Assign First Value Is nullptr
-    this->prev = nullptr; // Assign First Value Is nullptr
-  }
-};
-
+template <class D>
 class Double_Linked_List
 {
+  class Double_Node
+  {
+  public:
+    D data;            // Data Initial By User
+    Double_Node *next; // Pointer To Reference Next Node
+    Double_Node *prev; // Pointer To Reference Pervious Node
+
+    Double_Node(D value)
+    {                       // Constructor To Assign Value In Function  For Data In Double_Node
+      this->data = value;   // Assign Value In Function For Data In Class
+      this->next = nullptr; // Assign First Value Is nullptr
+      this->prev = nullptr; // Assign First Value Is nullptr
+    }
+  };
+
 private:
   Double_Node *head = nullptr; // Head pointer To Reference Always In First Element In Double Linked List
   Double_Node *tail = nullptr; // Tail pointer To Reference Always In Last Element In Double Linked List
@@ -173,7 +219,7 @@ private:
   }
 
 public:
-  void Insert_Node_At_Beginning(double value) // Function To Add Data In First
+  void Insert_Node_At_Beginning(D value) // Function To Add Data In First
   {
     Double_Node *New_Node = new Double_Node(value); // Create New Node And Assign Value For Data
     if (head == nullptr)                            // Check If List Empty Or No
@@ -192,7 +238,7 @@ public:
 
   // ====================================
 
-  void Insert_Node_At_End(double value)
+  void Insert_Node_At_End(D value)
   {                                                 // Function To Add Data In Last
     Double_Node *New_Node = new Double_Node(value); // Create New Node And Assign Value For Data
     if (this->head == nullptr)                      // Check If List Empty Or No
@@ -212,7 +258,7 @@ public:
 
   // ====================================
 
-  void Remove_Node(double value)
+  void Remove_Node(D value)
   {                      // Function To Remove Node
     if (head == nullptr) // Check If List Is Empty Or No
     {
@@ -313,27 +359,38 @@ public:
 
 int main()
 {
-  Linked_List My_List;
+  Linked_List<int> My_List;
   cout << "Start\n";
 
   My_List.Insert_Node_At_Beginning(3);
   My_List.Insert_Node_At_Beginning(2);
   My_List.Insert_Node_At_Beginning(1);
   My_List.Insert_Node_At_End(4);
+  My_List.Insert_Node_At_End(8);
 
-  My_List.Print_List();
-
-  My_List.Remove_Node(1);
-  My_List.Remove_Node(2);
-  My_List.Remove_Node(3);
-  My_List.Remove_Node(4);
+  My_List.Insert_At_Position(5, 4);
+  My_List.Insert_At_Position(6, 5);
+  My_List.Insert_At_Position(10, 6);
 
   My_List.Print_List();
   cout << "Length: " << My_List.Get_Length() << "\n";
 
   cout << "===================\n";
 
-  Double_Linked_List New_List;
+  My_List.Remove_Node(8);
+  My_List.Remove_Node(1);
+  My_List.Remove_Node(4);
+  My_List.Remove_Node(5);
+
+  My_List.Print_List();
+
+  cout << "Length: " << My_List.Get_Length() << "\n";
+  cout << "===================\n";
+
+  // My_List.Print_List();
+  cout << "===================\n";
+
+  Double_Linked_List<int> New_List;
 
   New_List.Insert_Node_At_Beginning(3);
   New_List.Insert_Node_At_Beginning(2);
@@ -341,6 +398,8 @@ int main()
   New_List.Insert_Node_At_End(4);
 
   New_List.Print_List();
+  cout << "Length: " << New_List.Get_Length() << "\n";
+  cout << "===================\n";
 
   New_List.Remove_Node(1);
   New_List.Remove_Node(2);
